b20cd32fdd1d5b8ae04575b06bce1fffe8712b9a
gferrandez <gefs48@gmail.com>
2017-06-13 17:07:19 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,84 @@
+function pag_Me(){ 
+window.location.href="Me.html"; 
+}
+
+function pag_Contact(){ 
+window.location.href="Contact.html"; 
+}
+
+function pag_Home(){ 
+window.location.href="Home.html"; 
+}
+
+
+function pag_Modeling(){ 
+    setTimeout (function(){
+window.location.href="Modeling.html";
+}, 900);
+}
+
+function pag_Ilumination(){
+    setTimeout (function(){  
+window.location.href="Ilumination.html"; 
+}, 900);
+}
+
+function pag_Ilustrator(){ 
+    setTimeout (function(){
+window.location.href="Ilustrator.html"; 
+}, 900);
+}
+
+function pag_3Danimation(){
+    setTimeout (function(){
+window.location.href="3Danimation.html"; 
+}, 900);
+}
+
+function pag_Texturing(){
+    setTimeout (function(){
+window.location.href="Texturing.html"; 
+},900);
+}
+
+function pag_Css(){ 
+     setTimeout (function(){
+window.location.href="Css.html"; 
+}, 900);
+}
+
+
+
+function Modelatge(){
+document.getElementById('Modelatges').className = " modalitatsLI_Mod_final";
+ 
+    }
+
+
+function Texturing(){
+document.getElementById('Texturings').className = " modalitatsLI_Tx_final";
+ 
+    }
+
+function Ilustracio(){
+document.getElementById('Ilustracio').className = " modalitatsLI_Ilus_final";
+ 
+    }
+
+function Animacio(){
+document.getElementById('Animacio').className = " modalitatsLI_Anim_final";
+ 
+    }
+
+function Css(){
+document.getElementById('Css').className = " modalitatsLI_Css_final";
+ 
+    }
+
+function Iluminacio(){
+document.getElementById('Iluminacio').className = " modalitatsLI_Ilum_final";
+ 
+    }
+
+
+
